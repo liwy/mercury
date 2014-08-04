87eacc8f4752b957c2ee0ebfa45fb48d6db65c3d
wli <wli@localhost>
2014-08-04 11:28:49 -0400

    NG-23756

---
+++ common/src/test/resources/init_table.sql
@@ -0,0 +1,10 @@
+drop table my_table if exists;
+
+SET DATABASE SQL SYNTAX ORA TRUE;
+
+-- CREATE SEQUENCE TRACKING_CONFIG_SEQ AS INTEGER START WITH 5 INCREMENT BY 1;
+
+CREATE TABLE my_table (
+	ID		int not null primary key,
+....
+);
